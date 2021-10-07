5958cc7b0661ab63e6df6db2f5b1014a9f8972bc
pdh93621 <hyenp1993@gmail.com>
2021-10-07 17:21:17 +0900

    no message

---
+++ Lv2_Right_Bracket.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer;
+    stack<char> stk;
+    
+    if (s[0] != '('){
+        return false;
+    }
+
+    for(int i = 0; i < s.size(); i++){
+        if(!stk.empty() and stk.top() == '(' and s[i] == ')'){
+            stk.pop();
+        }
+        else{
+            stk.push(s[i]);
+        }
+    }
+
+    return stk.empty();
+}
+
+int main(){
+
+    string s = "(()("	;
+
+    cout << (bool)solution(s) << endl;
+
+    return 0;
+}
