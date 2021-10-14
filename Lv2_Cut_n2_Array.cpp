3eba8e9aaf4bbcbbd0567492c245772c8ae71023
pdh93621 <hyenp1993@gmail.com>
2021-10-14 19:07:53 +0900

    no message

--- Lv2_Cut_n2_Array.cpp
+++ Lv2_Cut_n2_Array.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-
-    
-
-
+    int row;
+    int column;
+    for(long long i = left; i <= right; i++){
+        answer.push_back(max(i / n, i % n) + 1);
+    }
 
     return answer;
 }
 
 int main(){
 
-    int n = 4;
-    long long left = 7;
-    long long right = 14;
+    int n = 3;
+    long long left = 2;
+    long long right = 5;
 
-    vector<int> result = solution(4, 7 ,14);
+    vector<int> result = solution(n, left, right);
 
     for(int i = 0; i < result.size(); i++){
         cout << result[i] << " ";
