d3c4a1f50b76650138a7969e6daced403808eedd
pdh93621 <hyenp1993@gmail.com>
2021-10-14 17:47:15 +0900

    no message

---
+++ Lv2_Cut_n2_Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+
+    
+
+
+
+    return answer;
+}
+
+int main(){
+
+    int n = 4;
+    long long left = 7;
+    long long right = 14;
+
+    vector<int> result = solution(4, 7 ,14);
+
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
